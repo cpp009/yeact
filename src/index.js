6075d9173507fb5d33815d39975346da8e507b00
cpp009 <1006767658@qq.com>
2021-10-16 18:04:43 +0800

    webpack serve

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log('Hello Yeact')
+console.log('Hello Yeact...')
+
+
+
+
+// Representation of JSX
+function createElement(type, config, children) {
+
+  return {
+    type,
+    key: config?.key,
+    props: {
+      children
+    }
+  }
+}
+
+
+// Render JSX element
+function render(
+  element,
+  container
+) {
+
+  // element container to fiber
+  const root = createRootFromContainer(container);
+  // insert element to container fiber
+  updateContainer(root, element)
+}
+
+
+function createRootFromContainer(container) {
+  // trasform DOM container to fiber
+}
+
+function updateContainer(root, element) {
+
+}
+
